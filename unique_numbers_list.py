6d17ccc27d999ea5c4f522f804f52b82d065f864
joao <joaopedroq333@gmail.com>
2025-04-28 14:23:43 -0300

    add sort

--- unique_numbers_list.py
+++ unique_numbers_list.py
@@ -1,25 +1,20 @@
-# unique_numbers_list.py
-# Program to add unique numbers to a list.
-# It does not allow duplicate numbers and lets the user decide whether to continue.
-
 numbers = []
 
 while True:
     n = int(input('Enter a number: '))
-    
     if n not in numbers:
         numbers.append(n)
         print('Number added successfully!')
     else:
         print('ERROR! Number already added.')
 
-    continue_input = input('Do you want to continue? [Y/N] ').strip().upper()
+    continue_input = str(input('Do you want to continue? [Y/N] '))
+    while continue_input not in 'YyNn':
+        continue_input = str(input('Do you want to continue? [Y/N] '))
     
-    while continue_input not in ('Y', 'N'):
-        continue_input = input('Invalid option. Do you want to continue? [Y/N] ').strip().upper()
-    
-    if continue_input == 'N':
+    if continue_input in 'Nn':
         break
 
+numbers.sort()
 print('-=' * 30)
-print(f'You entered the values: {sorted(numbers)}')
+print(f'You entered the values: {numbers}')
